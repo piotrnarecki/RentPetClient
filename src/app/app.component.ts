c51d43b474165d7af7dbf8134b870f745e1b42d4
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-03-06 09:23:55 +0100

    dodano podstawy routingu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,HomeComponent, HeaderComponent,FooterComponent,CatalogComponent],
+  imports: [CommonModule,HomeComponent, HeaderComponent,FooterComponent,CatalogComponent,RouterOutlet,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
