16cdf5df6cd36b9c6982f95293dfc37d3cefebf8
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-03-06 08:42:14 +0100

    poprawiono header i footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+
 import { CatalogComponent } from './catalog/catalog.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
