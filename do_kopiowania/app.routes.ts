a6f56b99a4f30e1874dce78e672fae4690e53b18
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-03-06 11:22:01 +0100

    dodano routes do skopiowania

---
+++ do_kopiowania/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent, title: "Home - Rent-Pet" },
+    { path: 'catalog', component: CatalogComponent, title: "Cat-a-log - Rent-Pet" },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  ];;
